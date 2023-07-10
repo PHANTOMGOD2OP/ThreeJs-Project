2be86a6289baf3fc60b9bc3bd112712b056465c8
PHANTOMGOD2OP <achharyavedant@gmail.com>
2023-07-10 13:44:29 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-
+import bodyParser from 'bodyParser';
 import dalleRoutes from './routes/dalle.routes.js';
 
 dotenv.config();
