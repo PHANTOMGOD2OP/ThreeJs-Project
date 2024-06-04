4925c0b21ea64c2b4448d0cfc2c3416866c70d58
VEDANT ACHHARYA <achharyavedant@gmail.com>
2024-06-05 04:33:47 +0530

    Resolved merge conflicts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 module.exports = {
-=======
-export default {
->>>>>>> 4d6fb1ddd87618deb14f3f18328918530ccfd088
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
