4d6fb1ddd87618deb14f3f18328918530ccfd088
VEDANT ACHHARYA <achharyavedant@gmail.com>
2024-06-05 03:56:08 +0530

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://project-backend-3d.onrender.com', {
+      const response = await fetch('https://threejs-project-oolt.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
