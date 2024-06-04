f55cb9e70d27f4e5aa9b7b8ce6f1f3db947a2f6f
VEDANT ACHHARYA <achharyavedant@gmail.com>
2024-06-05 03:20:18 +0530

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://threejs-project-oolt.onrender.com/api/v1/dalle",
   },
 };
 
